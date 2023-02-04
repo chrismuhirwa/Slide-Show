d19680a6cd7012ff0c51915973a7bbeffec93c30
Chris Muhirwa <chris.muhirwa@yahoo.com>
2023-02-04 14:14:50 -0600

    slideshow

---
+++ app.js
@@ -0,0 +1,6 @@
+// let image = document.getElementsByClassName("images");
+// let images = ["poster1.png","poster2.png","poster3.png"]
+// setInterval(function(){
+//     let random = Math.floor(Math.random() * 3);
+//     image.src = image[random];
+// }, 800);
